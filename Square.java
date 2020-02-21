8c544d5febc9404e12e2bb0979a15e9ab5c54e8f
rlyyah <tmakowski1337@gmail.com>
2020-02-21 22:23:50 +0100

    Add contructor, xPos and Ypos field of square, mark method

--- Square.java
+++ Square.java
@@ -1,4 +1,24 @@
 public class Square{
 
+    private Integer xPos;
+    private Integer yPos;
+    private Boolean isHit;
+    private Boolean isShip;
+
+    public void mark(){
+        isHit = true;
+    }
+
+    public Square(Integer xPos, Integer yPos){
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public String toString(){
+
+        String square = "O";
+        
+        return square;
+    }
 
 }
