7629a8e3767ef6f46183ccf531eb54da60f7eef0
rlyyah <tmakowski1337@gmail.com>
2020-02-22 00:03:59 +0100

    Fix toString, add addShip method

--- Ocean.java
+++ Ocean.java
@@ -21,11 +21,34 @@ public class Ocean {
         }
     }
 
+    public void addShip(Integer y, Integer x, Integer type, Character dimension){
+
+        Ship ship = new Ship(y, x, type, dimension);
+
+        if(dimension == 'h'){
+            for(int i = 0; i < type; i++){
+                ocean.get(y*10+x+i).makeShip();
+            }
+        }else{
+            for(int i = 0; i<type; i++){
+                ocean.get((y+i)*10+x).makeShip();
+            }
+        }
+
+    }
+
+    public String quess(int yPos, int xPos){
+        String msg = "Miss";
+        Integer squareNum = yPos * 10 + xPos;
+        ocean.get(squareNum).mark();
+        return msg;
+    }
+
     public String toString(){
         StringBuffer sb = new StringBuffer();
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWSYZ";
         
-        //HERE COMES A B C... TOP NOTATION
+        // HERE COMES A B C... TOP(horizontal) NOTATION
         sb.append("  ");
         for(int i=0;i<width;i++){
             sb.append(" "+ String.valueOf(alphabet.charAt(i)));
@@ -33,18 +56,28 @@ public class Ocean {
         sb.append("\n");
         
         for(int i=0;i<height;i++){
-            
+            // This one is for the left(vertical) notation 
             if(String.valueOf(i+1).length()>1){
                 sb.append(String.valueOf(i+1) + " ");
             }else{
                 sb.append(" " + String.valueOf(i+1) + " ");
             }
             for(int j=0;j<width;j++){
-                sb.append(ocean.get(i+j).toString());
+                // HERE starts the magic of map creation!
+                sb.append(ocean.get(i*10+j).toString());
                 sb.append(" ");
             }
             sb.append("\n");
         }
         return sb.toString();
-    }
+    } 
+    
+    /*public String toString(){
+        StringBuffer sb = new StringBuffer();
+        for(Square s: ocean){
+            sb.append(s.toString());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }*/
 }
