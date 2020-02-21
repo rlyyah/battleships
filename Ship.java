638b64a6af8faea2bd098208c26151591140bdcf
rlyyah <tmakowski1337@gmail.com>
2020-02-22 00:04:56 +0100

    Add contructor

--- Ship.java
+++ Ship.java
@@ -1,3 +1,15 @@
 public class Ship{
-    
+
+    private Character dimension;
+    private Integer type;
+    private Integer xStart;
+    private Integer yStart;
+
+    public Ship(Integer y, Integer x, Integer type, Character dimension){
+        this.yStart = y;
+        this.xStart = x;
+        this.type = type;
+        this.dimension = dimension;
+    }
+
 }
