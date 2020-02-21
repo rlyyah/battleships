2f634862bb61cfc76b89c39f558a3d9bc02926ea
rlyyah <tmakowski1337@gmail.com>
2020-02-22 00:04:41 +0100

    add mark as ship method, toString now show if square is a part of a ship

--- Square.java
+++ Square.java
@@ -6,18 +6,39 @@ public class Square{
     private Boolean isShip;
 
     public void mark(){
-        isHit = true;
+        this.isHit = true;
     }
 
-    public Square(Integer xPos, Integer yPos){
+    public void makeShip(){
+
+        // TODO:
+        //      ships cannot overlap eachother
+        //      ships cannot touch another ship
+        //      ships cannot hang of the map
+
+        this.isShip = true;
+    }
+
+    public Square(Integer yPos, Integer xPos){
         this.xPos = xPos;
         this.yPos = yPos;
+        this.isHit = false;
+        this.isShip = false;
     }
 
     public String toString(){
 
+        // TODO:
+        //      DIFFERENT COLORS EG.: Ocean O-blue, Ship S-Orange, Hit(missed) X-white, Hit(not missed) X-Red
+
+        // String square = "Y:" + String.valueOf(this.yPos) + " | X:" + String.valueOf(this.xPos) + " | " + String.valueOf(isHit);
         String square = "O";
-        
+        if(isShip){
+            square = "S";
+        }
+        if(isHit){
+            square = "X";
+        }
         return square;
     }
 
