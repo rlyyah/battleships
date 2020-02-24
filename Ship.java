a104de6c86aabb0df2c515901cc20519e660a9fb
rlyyah <tmakowski1337@gmail.com>
2020-02-24 22:33:35 +0100

    add make ship method

--- Ship.java
+++ Ship.java
@@ -1,15 +1,24 @@
-public class Ship{
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ship {
 
     private Character dimension;
     private Integer type;
     private Integer xStart;
     private Integer yStart;
+    private List<Square> shipPositions;
 
     public Ship(Integer y, Integer x, Integer type, Character dimension){
         this.yStart = y;
         this.xStart = x;
         this.type = type;
         this.dimension = dimension;
+        shipPositions = new LinkedList<>();
+    }
+
+    public void makeShip(Square s){
+        shipPositions.add(s);
     }
 
 }
