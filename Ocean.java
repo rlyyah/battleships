2e78451485413236f616cc82d02b5bcc091b9842
rlyyah <tmakowski1337@gmail.com>
2020-02-24 22:30:32 +0100

    Fix makeship() so that it does let creater ship on another ship nor in the border squars

--- Ocean.java
+++ Ocean.java
@@ -1,15 +1,18 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Ocean {
     
     private List<Square> ocean;
+    private List<Square> unableToBeBuild;
     private Integer height = 10;
     private Integer width = 10;
     
     public Ocean(){
 
         ocean = new LinkedList<Square>();
+        unableToBeBuild = new LinkedList<>();
         
         for(int i = 0;i<height; i++){
             for(int j = 0; j<width; j++){
@@ -21,19 +24,124 @@ public class Ocean {
         }
     }
 
-    public void addShip(Integer y, Integer x, Integer type, Character dimension){
+    public String addShip(Integer y, Integer x, Integer type, Character dimension){
 
         Ship ship = new Ship(y, x, type, dimension);
+        List<Square> shipInProgress = new ArrayList<>();
+        List<Integer> borders = new ArrayList<>();
 
         if(dimension == 'h'){
             for(int i = 0; i < type; i++){
-                ocean.get(y*10+x+i).makeShip();
+                try{
+                    shipInProgress.add(ocean.get(y*10+x+i));
+                }catch(Exception e){
+                    return "Ship cannot be build here!";
+                }
+                
             }
         }else{
-            for(int i = 0; i<type; i++){
-                ocean.get((y+i)*10+x).makeShip();
+            for(int i = 0; i < type; i++){
+                try{
+                    shipInProgress.add(ocean.get((y+i)*10+x));
+                }catch(Exception e){
+                    return "Ship cannot be build here!";
+                }
             }
         }
+
+        if(!unableToBeBuild.isEmpty()){
+            for(Square s: unableToBeBuild){
+                if(shipInProgress.contains(s)){
+                    return "Ship cannot be build here"; 
+                }
+           
+            }
+        }
+
+        for(Square s: shipInProgress){
+            s.makeShip();
+            ship.makeShip(s);
+        }
+
+        Integer index = 0;
+        for(Square s: shipInProgress){
+
+            // Here comes inability to build over another ship
+            unableToBeBuild.add(s);
+            
+            // Here comes prohibition to build nearby another ship LOTS OF CODE :c 
+            // TODO:
+            //      make it into the function and/or maybe shorten it
+            //      put borders into different list maybe
+            Integer yPos = s.getyPos();
+            Integer xPos = s.getxPos();
+
+            if(dimension == 'h'){
+
+                try{
+                    if(index == 0){
+                        for(int i = -1; i<2;i++){
+                            unableToBeBuild.add(ocean.get((yPos+i)*10+xPos-1));
+                        }
+                    }
+                    if(index == shipInProgress.size()-1){
+                        for(int i=-1;i<2;i++){
+                            unableToBeBuild.add(ocean.get((yPos+i)*10+xPos+1));  
+                        }
+                    }
+                }catch(Exception e){
+
+                }
+                try{
+                    unableToBeBuild.add(ocean.get((yPos+1)*10+xPos));
+                }catch(Exception e){
+                    //System.out.println("upper border cannot be build");
+                }
+                try{
+                    unableToBeBuild.add(ocean.get((yPos-1)*10+xPos));
+                }catch(Exception e){
+                    //System.out.println("lower border cannot be build");
+                }
+
+                
+                
+
+            }else{
+
+                try{
+                    if(index == 0){
+                        for(int i = -1; i<2;i++){
+                            unableToBeBuild.add(ocean.get((yPos-1)*10+xPos+i));
+                        }
+                    }
+                    if(index == shipInProgress.size()-1){
+                        for(int i=-1;i<2;i++){
+                            unableToBeBuild.add(ocean.get((yPos+1)*10+xPos+i));  
+                        }
+                    }
+                }catch(Exception e){
+
+                }
+
+                try{
+                    unableToBeBuild.add(ocean.get(yPos*10+(x-1)));
+                }catch(Exception e){
+
+                }
+                try{
+                    unableToBeBuild.add(ocean.get(yPos*10+(x+1)));
+                }catch(Exception e){
+
+                }
+                
+                
+            }
+            
+            index++;
+        }
+
+
+        return "Ship has been built";
 
     }
 
