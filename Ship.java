c4b58b0dbfc499488cba30efc45651f98af30314
rlyyah <tmakowski1337@gmail.com>
2020-03-03 11:10:30 +0100

    add some getters and setters for isHit

--- Ship.java
+++ Ship.java
@@ -8,6 +8,7 @@ public class Ship {
     private Integer xStart;
     private Integer yStart;
     private List<Square> shipPositions;
+    private Boolean isDead;
 
     public Ship(Integer y, Integer x, Integer type, Character dimension){
         this.yStart = y;
@@ -15,10 +16,38 @@ public class Ship {
         this.type = type;
         this.dimension = dimension;
         shipPositions = new LinkedList<>();
+        this.isDead = false;
     }
 
     public void makeShip(Square s){
         shipPositions.add(s);
     }
+
+    public Boolean checkIfDead(){
+        for(Square s: this.shipPositions){
+            if(!s.getIsHit()){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Square> getShipPositions() {
+        return shipPositions;
+    }
+
+    public void setShipPositions(List<Square> shipPositions) {
+        this.shipPositions = shipPositions;
+    }
+
+    public Character getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(Character dimension) {
+        this.dimension = dimension;
+    }
+
+
 
 }
